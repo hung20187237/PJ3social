ee4e05a54c3ec5a2e4d705225d993683c5643de5
tranduchung308 <tranduchung308@gmail.com>
2022-08-26 12:14:34 +0700

    aZ

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -36,7 +36,6 @@ export default function Topbar(socket) {
             setNotifications(res.data.sort((p1, p2) => {
               return new Date(p2.createdAt) - new Date(p1.createdAt);
             }));
-            setCountNewNotifications(res.data.length)
           } catch (err) {
             console.log(err);
           }
